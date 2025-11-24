848b8bf9f5e9ba7b376812727a5c201d76c17284
wayyne <iwayyne@live.com>
2025-11-23 19:19:36 -0500

    Update wizard.sh
    added wget calls to fetch weights from Zenodo and decompress them

--- env/wizard.sh
+++ env/wizard.sh
@@ -15,4 +15,24 @@ pip install -U pip
 pip install numpy torch esm tqdm httpx colorama
 
 #download wts from zenodo
+cd wts
 
+#ligcysS
+wget https://zenodo.org/records/17693548/files/ligcysS_v1.tar.xz
+tar -xJf ligcysS_v1.tar.xz
+
+#ligcysA
+wget https://zenodo.org/records/17693548/files/ligcysA_v1.tar.xz
+tar -xJf ligcysA_v1.tar.xz
+
+#ligbind
+wget https://zenodo.org/records/17693713/files/ligbind_v1.tar.xz
+tar -xJf ligbind_v1.tar.xz
+
+#ssbind
+wget https://zenodo.org/records/17693474/files/ssbind_v1.tar.xz
+tar -xJf ssbind_v1.tar.xz
+
+#znbind
+wget https://zenodo.org/records/17692131/files/znbind_v1.tar.xz
+tar -xJf znbind_v1.tar.xz
