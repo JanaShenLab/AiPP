c38c83a1098ed7fc201af40d5eb6fd2ee47063f2
Wayyne <iwayyne@live.com>
2025-11-23 19:53:41 -0500

    updt to wizard

--- env/wizard.sh
+++ env/wizard.sh
@@ -18,21 +18,39 @@ pip install numpy torch esm tqdm httpx colorama
 cd wts
 
 #ligcysS
+echo "Fetching LigCys-S weights: this could take a few minutes"
 wget https://zenodo.org/records/17693548/files/ligcysS_v1.tar.xz
+echo "Extracting LigCys-S weights: this could take a few minutes"
 tar -xJf ligcysS_v1.tar.xz
 
 #ligcysA
+echo "Fetching LigCys-A weights: this could take a few minutes"
 wget https://zenodo.org/records/17693548/files/ligcysA_v1.tar.xz
+echo "Extracting LigCys-A weights: this could take a few minutes"
 tar -xJf ligcysA_v1.tar.xz
 
 #ligbind
+echo "Fetching LigBind weights: this could take a few minutes"
 wget https://zenodo.org/records/17693713/files/ligbind_v1.tar.xz
+echo "Extracting LigBind weights: this could take a few minutes"
 tar -xJf ligbind_v1.tar.xz
 
 #ssbind
+echo "Fetching SSBind weights: this could take a few minutes"
 wget https://zenodo.org/records/17693474/files/ssbind_v1.tar.xz
+echo "Extracting SSBind weights: this could take a few minutes"
 tar -xJf ssbind_v1.tar.xz
 
 #znbind
+echo "Fetching ZNBind weights: this could take a few minutes"
 wget https://zenodo.org/records/17692131/files/znbind_v1.tar.xz
+echo "Extracting ZNBind weights: this could take a few minutes"
 tar -xJf znbind_v1.tar.xz
+
+#housekeeping
+echo "Peforming housekeeping"
+rm ligcysS_v1.tar.xz
+rm ligcysA_v1.tar.xz
+rm ligbind_v1.tar.xz
+rm ssbind_v1.tar.xz
+rm znbind_v1.tar.xz
