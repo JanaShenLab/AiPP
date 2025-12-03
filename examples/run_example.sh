85fc4d4f9ef760197c05878229c6f6e3cb536ff5
Wayyne <iwayyne@live.com>
2025-12-02 23:39:32 -0500

    added examples

---
+++ examples/run_example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#activate the environment (assuming wizard was ran successfully)
+source ../env/AiPP/bin/activate
+
+#run AiPP on PTPN6
+python aippCLI.py \
+  --fasta ptpn6.fasta \
+  --id ptpn6 \
+  --out ptpn6.tsv
